54458d118b48bd582eea35ff933da2da4e6a4889
Satyajeet Namawar <namawar.satyajeet@gmail.com>
2023-08-10 19:05:29 +0530

    Add project files.

---
+++ LoginWithoutIDCore/Data/LoginDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoginWithoutIDCore.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginWithoutIDCore.Data
+{
+    public class LoginDbContext: DbContext
+    {
+        public LoginDbContext(DbContextOptions<LoginDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+        public virtual DbSet<User> Menus { get; set; }
+    }
+}
---
+++ LoginWithoutIDCore/Models/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginWithoutIDCore.Models.Domain
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string City { get; set; }
+
+    }
+}
